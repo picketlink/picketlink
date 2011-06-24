172aaa9faf8dfdb014e1a61ac36e14405970db00
Anil Saldhana <anil.saldhana@jboss.com>
2011-06-24 18:10:38 +0000

    unneeded system.out
    git-svn-id: https://svn.jboss.org/repos/picketlink/federation/trunk@1032 cbddccc4-e45a-42eb-be77-0d76e4b45d38

--- src/test/java/org/picketlink/test/identity/federation/bindings/workflow/SAML2LogoutTomcatWorkflowUnitTestCase.java
+++ src/test/java/org/picketlink/test/identity/federation/bindings/workflow/SAML2LogoutTomcatWorkflowUnitTestCase.java
@@ -21,7 +21,9 @@
  */
 package org.picketlink.test.identity.federation.bindings.workflow;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -34,8 +36,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
-
-import junit.framework.TestCase;
 
 import org.apache.catalina.deploy.LoginConfig;
 import org.apache.catalina.realm.GenericPrincipal;
@@ -65,18 +65,22 @@ import org.picketlink.test.identity.federation.bindings.mock.MockCatalinaSession
 @SuppressWarnings("unused")
 public class SAML2LogoutTomcatWorkflowUnitTestCase
 {
-   private String profile = "saml2/logout";
+   private final String profile = "saml2/logout";
+
    private ClassLoader tcl;
 
-   private String IDP = "http://localhost:8080/idp/"; 
-   private String employee = "http://localhost:8080/employee/"; 
-   private String sales = "http://localhost:8080/sales/";
-   
+   private final String IDP = "http://localhost:8080/idp/";
 
-   private String RELAY_STATE_KEY = "RelayState=";
-   private String SAML_REQUEST_KEY = "SAMLRequest=";
-   private String SAML_RESPONSE_KEY = "SAMLResponse=";
-   
+   private final String employee = "http://localhost:8080/employee/";
+
+   private final String sales = "http://localhost:8080/sales/";
+
+   private final String RELAY_STATE_KEY = "RelayState=";
+
+   private final String SAML_REQUEST_KEY = "SAMLRequest=";
+
+   private final String SAML_RESPONSE_KEY = "SAMLResponse=";
+
    /**
     * Test that the SP Redirect Authenticator generates the logout request
     * to the IDP when there is a parameter "GLO" set to true
@@ -85,7 +89,7 @@ public class SAML2LogoutTomcatWorkflowUnitTestCase
     */
    @Test
    public void testSPLogOutRequestGeneration() throws Exception
-   { 
+   {
       MockCatalinaSession session = new MockCatalinaSession();
       session.setAttribute(GeneralConstants.PRINCIPAL_ID, new Principal()
       {
@@ -93,205 +97,182 @@ public class SAML2LogoutTomcatWorkflowUnitTestCase
          {
             return "anil";
          }
-       });
+      });
       List<String> rolesList = new ArrayList<String>();
       rolesList.add("manager");
       session.setAttribute(GeneralConstants.ROLES_ID, rolesList);
-      
 
       MockCatalinaContext context = new MockCatalinaContext();
       session.setServletContext(context);
-      
+
       //Let us feed the LogOutRequest to the SPFilter
       MockCatalinaContextClassLoader mclSPEmp = setupTCL(profile + "/sp/employee");
       Thread.currentThread().setContextClassLoader(mclSPEmp);
-      
+
       SPRedirectFormAuthenticator sp = new SPRedirectFormAuthenticator();
       sp.setContainer(context);
       sp.testStart();
-      
-      MockCatalinaRequest catalinaRequest = new MockCatalinaRequest(); 
+
+      MockCatalinaRequest catalinaRequest = new MockCatalinaRequest();
       MockCatalinaResponse response = new MockCatalinaResponse();
       MockCatalinaLoginConfig loginConfig = new MockCatalinaLoginConfig();
-      
+
       ByteArrayOutputStream filterbaos = new ByteArrayOutputStream();
       response.setWriter(new PrintWriter(filterbaos));
       catalinaRequest.setParameter(GeneralConstants.GLOBAL_LOGOUT, "true");
       sp.authenticate(catalinaRequest, response, loginConfig);
-      
+
       String redirectStr = response.redirectString;
-      String logoutRequest = redirectStr.substring(redirectStr.indexOf(SAML_REQUEST_KEY) +
-            SAML_REQUEST_KEY.length()) ;
-      
-      
-      InputStream stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutRequest); 
-      
+      String logoutRequest = redirectStr.substring(redirectStr.indexOf(SAML_REQUEST_KEY) + SAML_REQUEST_KEY.length());
+
+      InputStream stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutRequest);
+
       SAML2Request saml2Request = new SAML2Request();
-      LogoutRequestType lor = 
-         (LogoutRequestType) saml2Request.getRequestType(stream);
+      LogoutRequestType lor = (LogoutRequestType) saml2Request.getRequestType(stream);
       assertEquals("Match Employee URL", employee, lor.getIssuer().getValue());
    }
-   
 
    @Test
    public void testSAML2LogOutFromIDP() throws Exception
-   {  
+   {
       MockCatalinaSession session = new MockCatalinaSession();
-      
+
       MockCatalinaContextClassLoader mclIDP = setupTCL(profile + "/idp");
       Thread.currentThread().setContextClassLoader(mclIDP);
-      
+
       MockCatalinaContext catalinaContext = new MockCatalinaContext();
       session.setServletContext(catalinaContext);
-      
+
       IdentityServer server = this.getIdentityServer(session);
       catalinaContext.setAttribute("IDENTITY_SERVER", server);
-       
-      IDPWebBrowserSSOValve idp = new IDPWebBrowserSSOValve(); 
-      
+
+      IDPWebBrowserSSOValve idp = new IDPWebBrowserSSOValve();
+
       idp.setContainer(catalinaContext);
       idp.setSignOutgoingMessages(false);
       idp.setIgnoreIncomingSignatures(true);
-      idp.start(); 
-      
+      idp.start();
+
       //Assume that we already have the principal and roles set in the session
       MockCatalinaRealm realm = new MockCatalinaRealm("anil", "test", new Principal()
-      {   
+      {
          public String getName()
-         { 
+         {
             return "anil";
          }
       });
       List<String> roles = new ArrayList<String>();
       roles.add("manager");
       roles.add("employee");
-      
+
       List<String> rolesList = new ArrayList<String>();
       rolesList.add("manager");
-      
+
       MockCatalinaRequest request = new MockCatalinaRequest();
       session.clear();
-      request.setSession(session); 
-      
+      request.setSession(session);
+
       request.addHeader("Referer", sales);
 
-      GenericPrincipal genericPrincipal =new GenericPrincipal(realm, "anil", "test", roles) ; 
+      GenericPrincipal genericPrincipal = new GenericPrincipal(realm, "anil", "test", roles);
       request.setUserPrincipal(genericPrincipal);
-      
-      String samlMessage =  RedirectBindingUtil.deflateBase64Encode(createLogOutRequest(sales).getBytes()); 
-      request.setParameter("SAMLRequest", samlMessage);
-      
 
-      
+      String samlMessage = RedirectBindingUtil.deflateBase64Encode(createLogOutRequest(sales).getBytes());
+      request.setParameter("SAMLRequest", samlMessage);
+
       MockCatalinaResponse response = new MockCatalinaResponse();
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
       response.setWriter(new PrintWriter(baos));
-      
+
       // The IDP is preloaded with 2 participants : "http://localhost:8080/sales/"
       // and "http://localhost:8080/employee"
-      
+
       //Lets start the workflow with get 
       request.setMethod("GET");
-      idp.invoke(request, response);  
-      
-      String redirectStr = response.redirectString;
-      
-      System.out.println("RedirectStr from IDP=" + redirectStr);
+      idp.invoke(request, response);
 
-      String destination = redirectStr.substring(0,redirectStr.indexOf(SAML_REQUEST_KEY) -1);
-      String relayState = redirectStr.substring(redirectStr.indexOf(RELAY_STATE_KEY) +
-            RELAY_STATE_KEY.length()) ;
-      String logoutRequest = redirectStr.substring(redirectStr.indexOf(SAML_REQUEST_KEY) +
-            SAML_REQUEST_KEY.length(),redirectStr.indexOf(RELAY_STATE_KEY) - 1 ) ;
-       
-       
-      System.out.println("LogO=" + logoutRequest);
-      InputStream stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutRequest); 
-      
+      String redirectStr = response.redirectString;
+
+      String destination = redirectStr.substring(0, redirectStr.indexOf(SAML_REQUEST_KEY) - 1);
+      String relayState = redirectStr.substring(redirectStr.indexOf(RELAY_STATE_KEY) + RELAY_STATE_KEY.length());
+      String logoutRequest = redirectStr.substring(redirectStr.indexOf(SAML_REQUEST_KEY) + SAML_REQUEST_KEY.length(),
+            redirectStr.indexOf(RELAY_STATE_KEY) - 1);
+
+      InputStream stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutRequest);
+
       SAML2Request saml2Request = new SAML2Request();
-      LogoutRequestType lor = 
-         (LogoutRequestType) saml2Request.getRequestType(stream);
+      LogoutRequestType lor = (LogoutRequestType) saml2Request.getRequestType(stream);
       assertEquals("Match Employee URL", employee, destination);
       assertEquals("Destination exists", employee, lor.getDestination().toString());
-      
+
       //Let us feed the LogOutRequest to the SPFilter
       MockCatalinaContextClassLoader mclSPEmp = setupTCL(profile + "/sp/employee");
-      Thread.currentThread().setContextClassLoader(mclSPEmp);    
+      Thread.currentThread().setContextClassLoader(mclSPEmp);
 
       MockCatalinaContext context = new MockCatalinaContext();
       context.setRealm(realm);
       session.setServletContext(context);
-      
+
       SPRedirectFormAuthenticator sp = new SPRedirectFormAuthenticator();
       sp.setContainer(context);
-      sp.testStart(); 
-     
+      sp.testStart();
+
       request = new MockCatalinaRequest();
       request.setSession(session);
       request.setParameter("SAMLRequest", RedirectBindingUtil.urlDecode(logoutRequest));
       request.setParameter("RelayState", relayState);
-      
+
       MockCatalinaResponse filterResponse = new MockCatalinaResponse();
       ByteArrayOutputStream filterbaos = new ByteArrayOutputStream();
       filterResponse.setWriter(new PrintWriter(filterbaos));
-      
 
-      sp.authenticate(request, response, new LoginConfig());  
-      
+      sp.authenticate(request, response, new LoginConfig());
+
       redirectStr = response.redirectString;
-      
-      System.out.println("Redirect String from SP=" + redirectStr);
-      
-      destination = redirectStr.substring(0,redirectStr.indexOf(SAML_RESPONSE_KEY) -1);
-      relayState = redirectStr.substring(redirectStr.indexOf(RELAY_STATE_KEY) +
-            RELAY_STATE_KEY.length()) ;
-      assertNotNull("RelayState exists",relayState);
-      String logoutResponse = redirectStr.substring(redirectStr.indexOf(SAML_RESPONSE_KEY) +
-            SAML_RESPONSE_KEY.length(),redirectStr.indexOf(RELAY_STATE_KEY) - 1 ) ;
-       
-      System.out.println("Logout Response from SP=" + logoutResponse);
-      
-      stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutResponse); 
-      StatusResponseType statusResponse = 
-         (StatusResponseType) saml2Request.getSAML2ObjectFromStream(stream);
+
+      destination = redirectStr.substring(0, redirectStr.indexOf(SAML_RESPONSE_KEY) - 1);
+      relayState = redirectStr.substring(redirectStr.indexOf(RELAY_STATE_KEY) + RELAY_STATE_KEY.length());
+      assertNotNull("RelayState exists", relayState);
+      String logoutResponse = redirectStr.substring(
+            redirectStr.indexOf(SAML_RESPONSE_KEY) + SAML_RESPONSE_KEY.length(),
+            redirectStr.indexOf(RELAY_STATE_KEY) - 1);
+
+      stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutResponse);
+      StatusResponseType statusResponse = (StatusResponseType) saml2Request.getSAML2ObjectFromStream(stream);
       assertEquals("Match IDP URL", IDP, destination);
-      
+
       //Now the SP (employee app) has logged out and sending a status response to IDP
       Thread.currentThread().setContextClassLoader(mclIDP);
 
       session.clear();
       request.clear();
-      
+
       request.setMethod("GET");
       request.setSession(session);
       request.setUserPrincipal(genericPrincipal);
       request.setParameter("SAMLResponse", RedirectBindingUtil.urlDecode(logoutResponse));
       request.setParameter("RelayState", relayState);
-      
-      idp.invoke(request, response); 
 
-      destination = redirectStr.substring(0,redirectStr.indexOf(SAML_RESPONSE_KEY) -1);
-      relayState = redirectStr.substring(redirectStr.indexOf(RELAY_STATE_KEY) +
-            RELAY_STATE_KEY.length()) ;
-      logoutResponse = redirectStr.substring(redirectStr.indexOf(SAML_RESPONSE_KEY) +
-            SAML_RESPONSE_KEY.length(),redirectStr.indexOf(RELAY_STATE_KEY) - 1 ) ;
-       
-       
-      System.out.println("LogO=" + logoutResponse);
-      stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutResponse); 
-      
+      idp.invoke(request, response);
+
+      destination = redirectStr.substring(0, redirectStr.indexOf(SAML_RESPONSE_KEY) - 1);
+      relayState = redirectStr.substring(redirectStr.indexOf(RELAY_STATE_KEY) + RELAY_STATE_KEY.length());
+      logoutResponse = redirectStr.substring(redirectStr.indexOf(SAML_RESPONSE_KEY) + SAML_RESPONSE_KEY.length(),
+            redirectStr.indexOf(RELAY_STATE_KEY) - 1);
+
+      stream = RedirectBindingUtil.urlBase64DeflateDecode(logoutResponse);
+
       SAML2Response saml2Response = new SAML2Response();
-      statusResponse = 
-         (StatusResponseType) saml2Request.getSAML2ObjectFromStream(stream);
+      statusResponse = (StatusResponseType) saml2Request.getSAML2ObjectFromStream(stream);
       assertEquals("Match IDP URL", IDP, destination);
-      
+
       //Now we should have got a full success report from IDP
       MockCatalinaContextClassLoader mclSPSales = setupTCL(profile + "/sp/employee");
       Thread.currentThread().setContextClassLoader(mclSPSales);
       sp = new SPRedirectFormAuthenticator();
       sp.setContainer(context);
-      sp.testStart(); 
-      
+      sp.testStart();
+
       session.clear();
       request.clear();
       request.setSession(session);
@@ -299,12 +280,12 @@ public class SAML2LogoutTomcatWorkflowUnitTestCase
       request.setParameter("SAMLResponse", RedirectBindingUtil.urlDecode(logoutResponse));
       request.setParameter("RelayState", relayState);
       request.setContext(context);
-      
-      sp.authenticate(request, response, new LoginConfig()); 
-      
+
+      sp.authenticate(request, response, new LoginConfig());
+
       assertEquals(0, server.stack().getParticipants(session.getId()));
       assertEquals(0, server.stack().getNumOfParticipantsInTransit(session.getId()));
-      
+
       //Finally the session should be invalidated
       assertTrue(session.isInvalidated());
    }
@@ -312,32 +293,32 @@ public class SAML2LogoutTomcatWorkflowUnitTestCase
    private MockCatalinaContextClassLoader setupTCL(String resource)
    {
       tcl = Thread.currentThread().getContextClassLoader();
-      URL[] urls = new URL[] {tcl.getResource(resource)};
-      
+      URL[] urls = new URL[]
+      {tcl.getResource(resource)};
+
       MockCatalinaContextClassLoader mcl = new MockCatalinaContextClassLoader(urls);
       mcl.setDelegate(tcl);
       mcl.setProfile(resource);
       return mcl;
    }
-   
+
    private String createLogOutRequest(String url) throws Exception
    {
       SAML2Request samlRequest = new SAML2Request();
-      LogoutRequestType lot = 
-         samlRequest.createLogoutRequest(url);
+      LogoutRequestType lot = samlRequest.createLogoutRequest(url);
       StringWriter sw = new StringWriter();
       samlRequest.marshall(lot, sw);
       return sw.toString();
    }
-   
+
    //Get the Identity server with 2 participants
    private IdentityServer getIdentityServer(HttpSession session)
    {
       IdentityServer server = new IdentityServer();
       server.sessionCreated(new HttpSessionEvent(session));
-      
+
       server.stack().register(session.getId(), sales, false);
       server.stack().register(session.getId(), employee, false);
       return server;
-   }  
+   }
 }
