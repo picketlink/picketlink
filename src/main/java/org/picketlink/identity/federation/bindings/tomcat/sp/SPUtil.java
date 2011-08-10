bc1d4ce75fceb8522c1544b484018d0793121670
Anil Saldhana <anil.saldhana@jboss.com>
2011-08-10 16:42:40 +0000

    remove redundant method
    git-svn-id: https://svn.jboss.org/repos/picketlink/federation/trunk@1169 cbddccc4-e45a-42eb-be77-0d76e4b45d38

--- src/main/java/org/picketlink/identity/federation/bindings/tomcat/sp/SPUtil.java
+++ src/main/java/org/picketlink/identity/federation/bindings/tomcat/sp/SPUtil.java
@@ -22,7 +22,6 @@
 package org.picketlink.identity.federation.bindings.tomcat.sp;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.catalina.Context;
@@ -32,20 +31,7 @@ import org.picketlink.identity.federation.api.saml.v2.request.SAML2Request;
 import org.picketlink.identity.federation.core.ErrorCodes;
 import org.picketlink.identity.federation.core.exceptions.ConfigurationException;
 import org.picketlink.identity.federation.core.saml.v2.common.IDGenerator;
-import org.picketlink.identity.federation.core.saml.v2.common.StatementLocal;
-import org.picketlink.identity.federation.core.saml.v2.constants.JBossSAMLURIConstants;
-import org.picketlink.identity.federation.core.saml.v2.exceptions.AssertionExpiredException;
-import org.picketlink.identity.federation.core.saml.v2.util.AssertionUtil;
-import org.picketlink.identity.federation.saml.v2.assertion.AssertionType;
-import org.picketlink.identity.federation.saml.v2.assertion.AttributeStatementType;
-import org.picketlink.identity.federation.saml.v2.assertion.AttributeStatementType.ASTChoiceType;
-import org.picketlink.identity.federation.saml.v2.assertion.AttributeType;
-import org.picketlink.identity.federation.saml.v2.assertion.NameIDType;
-import org.picketlink.identity.federation.saml.v2.assertion.SubjectType;
 import org.picketlink.identity.federation.saml.v2.protocol.AuthnRequestType;
-import org.picketlink.identity.federation.saml.v2.protocol.ResponseType;
-import org.picketlink.identity.federation.saml.v2.protocol.ResponseType.RTChoiceType;
-import org.picketlink.identity.federation.saml.v2.protocol.StatusType;
 
 /**
  * Common code useful for a SP
@@ -71,62 +57,6 @@ public class SPUtil
       SAML2Request saml2Request = new SAML2Request();
       String id = IDGenerator.create("ID_");
       return saml2Request.createAuthnRequestType(id, serviceURL, identityURL, serviceURL);
-   }
-
-   /**
-    * Handle the SAMLResponse from the IDP
-    * @param request entire request from IDP
-    * @param responseType ResponseType that has been generated
-    * @param serverEnvironment tomcat,jboss etc
-    * @return   
-    * @throws AssertionExpiredException 
-    */
-   public Principal handleSAMLResponse(Request request, ResponseType responseType) throws ConfigurationException,
-         AssertionExpiredException
-   {
-      if (request == null)
-         throw new IllegalArgumentException(ErrorCodes.NULL_ARGUMENT + "request");
-      if (responseType == null)
-         throw new IllegalArgumentException(ErrorCodes.NULL_ARGUMENT + "response type");
-
-      StatusType statusType = responseType.getStatus();
-      if (statusType == null)
-         throw new IllegalArgumentException(ErrorCodes.NULL_VALUE + "Status Type from the IDP");
-
-      String statusValue = statusType.getStatusCode().getValue().toASCIIString();
-      if (JBossSAMLURIConstants.STATUS_SUCCESS.get().equals(statusValue) == false)
-         throw new SecurityException(ErrorCodes.IDP_AUTH_FAILED);
-
-      List<RTChoiceType> assertions = responseType.getAssertions();
-      if (assertions.size() == 0)
-         throw new IllegalStateException(ErrorCodes.NULL_VALUE + "No assertions in reply from IDP");
-
-      AssertionType assertion = assertions.get(0).getAssertion();
-      //Check for validity of assertion
-      boolean expiredAssertion = AssertionUtil.hasExpired(assertion);
-      if (expiredAssertion)
-         throw new AssertionExpiredException();
-
-      SubjectType subject = assertion.getSubject();
-
-      //JAXBElement<NameIDType> jnameID = (JAXBElement<NameIDType>) subject.getContent().get(0);
-      NameIDType nameID = (NameIDType) subject.getSubType().getBaseID();
-      String userName = nameID.getValue();
-      List<String> roles = new ArrayList<String>();
-
-      //Set it on a thread local for JBID integrators
-      StatementLocal.statements.set(assertion.getStatements());
-
-      //Let us get the roles
-      AttributeStatementType attributeStatement = (AttributeStatementType) assertion.getStatements().iterator().next();
-      List<ASTChoiceType> attList = attributeStatement.getAttributes();
-      for (ASTChoiceType obj : attList)
-      {
-         AttributeType attr = obj.getAttribute();
-         String roleName = (String) attr.getAttributeValue().get(0);
-         roles.add(roleName);
-      }
-      return this.createGenericPrincipal(request, userName, roles);
    }
 
    public Principal createGenericPrincipal(Request request, String username, List<String> roles)
