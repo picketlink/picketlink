7fd54e93291752e99c64929d605dc34b61b75809
Pedro Igor <pigor.craveiro@gmail.com>
2012-06-13 11:39:45 -0300

    [https://issues.jboss.org/browse/PLFED-333] PicketLink Logger Abstraction

---
+++ modules/container-bindings/picketlink-jbas7/src/main/java/org/picketlink/identity/federation/PicketLinkLoggerImpl.java
@@ -0,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.picketlink.identity.federation;
+
+import org.picketlink.identity.federation.core.exceptions.ProcessingException;
+
+
+/**
+ * @author <a href="mailto:psilva@redhat.com">Pedro Silva</a>
+ *
+ */
+public final class PicketLinkLoggerImpl implements PicketLinkLogger {
+
+    PicketLinkLoggerImpl() {
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.picketlink.identity.federation.PicketLinkLogger#nullArgument(java.lang.String)
+     */
+    public IllegalArgumentException nullArgument(String argument) {
+        return PicketLinkLoggerJBL.ROOT_LOGGER.nullArgument(argument);
+    }
+
+    /* (non-Javadoc)
+     * @see org.picketlink.identity.federation.PicketLinkLogger#shouldNotBeTheSame(java.lang.String)
+     */
+    public IllegalArgumentException shouldNotBeTheSame(String message) {
+        return PicketLinkLoggerJBL.ROOT_LOGGER.shouldNotBeTheSame(message);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.picketlink.identity.federation.PicketLinkLogger#resourceNotFound(java.lang.String)
+     */
+    public ProcessingException resourceNotFound(String resource) {
+        return PicketLinkLoggerJBL.ROOT_LOGGER.resourceNotFound(resource);
+    }
+
+    /* (non-Javadoc)
+     * @see org.picketlink.identity.federation.PicketLinkLogger#processingError(java.lang.Throwable)
+     */
+    public ProcessingException processingError(Throwable t) {
+        return PicketLinkLoggerJBL.ROOT_LOGGER.processingError(t);
+    }
+
+    /* (non-Javadoc)
+     * @see org.picketlink.identity.federation.PicketLinkLogger#unsupportedType(java.lang.String)
+     */
+    public RuntimeException unsupportedType(String name) {
+        return PicketLinkLoggerJBL.ROOT_LOGGER.unsupportedType(name);
+    }
+}
---
+++ modules/container-bindings/picketlink-jbas7/src/main/java/org/picketlink/identity/federation/PicketLinkLoggerJBL.java
@@ -0,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.picketlink.identity.federation;
+
+import org.jboss.logging.BasicLogger;
+import org.jboss.logging.Cause;
+import org.jboss.logging.Logger;
+import org.jboss.logging.Message;
+import org.jboss.logging.MessageLogger;
+import org.picketlink.identity.federation.core.exceptions.ProcessingException;
+
+/**
+ * @author <a href="mailto:psilva@redhat.com">Pedro Silva</a>
+ *
+ */
+@MessageLogger(projectCode = "PLFED")
+public interface PicketLinkLoggerJBL extends BasicLogger {
+
+    PicketLinkLoggerJBL ROOT_LOGGER = Logger.getMessageLogger(PicketLinkLoggerJBL.class, PicketLinkLoggerJBL.class.getPackage().getName());
+
+    @Message(id = 78, value = "Null Parameter: %s")
+    IllegalArgumentException nullArgument(String argument);
+
+    @Message(id = 16, value = "Should not be the same: %s")
+    IllegalArgumentException shouldNotBeTheSame(String message);
+
+    @Message(id = 18, value = "Resource not found: %s")
+    ProcessingException resourceNotFound(String fileName);
+
+    @Message(id = 102, value = "Processing Exception.")
+    ProcessingException processingError(@Cause Throwable t);
+
+    @Message(id = 69, value = "Parser: Type not supported: %s")
+    RuntimeException unsupportedType(String name);
+    
+}
