ede58f17dece29bb98eb52202fdb50f438d8546b
Shane Bryzak <sbryzak@gmail.com>
2012-10-15 09:42:56 +1000

    minor

---
+++ core/impl/src/main/java/org/picketlink/producer/IdentityManagerProducer.java
@@ -0,0 +1,24 @@
+package org.picketlink.producer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.internal.DefaultIdentityManager;
+import org.picketlink.idm.jpa.internal.JPAIdentityStore;
+
+
+/**
+ * 
+ * @author Shane Bryzak
+ */
+public class IdentityManagerProducer {
+    
+    // FIXME basic implementation just to get started, we need to rewrite this with proper configuration
+    
+    @Produces @ApplicationScoped
+    public IdentityManager createIdentityManager() {
+        return new DefaultIdentityManager(new JPAIdentityStore());        
+    }
+    
+}
