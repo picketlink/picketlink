0db936dc38756a3e4d04c38369b8c831fd763b8d
pedroigor <pigor.craveiro@gmail.com>
2012-12-22 02:15:29 -0200

    Improvements to the LDAPIdentitystore.

--- idm/impl/src/test/java/org/picketlink/test/idm/suites/LDAPIdentityStoreTestSuite.java
+++ idm/impl/src/test/java/org/picketlink/test/idm/suites/LDAPIdentityStoreTestSuite.java
@@ -34,7 +34,17 @@ import org.picketlink.idm.internal.DefaultIdentityStoreInvocationContextFactory;
 import org.picketlink.idm.ldap.internal.LDAPConfiguration;
 import org.picketlink.idm.ldap.internal.LDAPConfigurationBuilder;
 import org.picketlink.idm.ldap.internal.LDAPIdentityStore;
+import org.picketlink.test.idm.ApplicationRegistrationTestCase;
+import org.picketlink.test.idm.CredentialManagementTestCase;
+import org.picketlink.test.idm.GroupManagementTestCase;
+import org.picketlink.test.idm.GroupQueryTestCase;
+import org.picketlink.test.idm.RoleManagementTestCase;
+import org.picketlink.test.idm.RoleQueryTestCase;
+import org.picketlink.test.idm.UserGroupRoleRelationshipTestCase;
+import org.picketlink.test.idm.UserGroupsRelationshipTestCase;
+import org.picketlink.test.idm.UserManagementTestCase;
 import org.picketlink.test.idm.UserQueryTestCase;
+import org.picketlink.test.idm.UserRolesRelationshipTestCase;
 import org.picketlink.test.idm.runners.IdentityManagerRunner;
 import org.picketlink.test.idm.runners.TestLifecycle;
 
@@ -47,7 +57,9 @@ import org.picketlink.test.idm.runners.TestLifecycle;
  *
  */
 @RunWith(IdentityManagerRunner.class)
-@SuiteClasses({ UserQueryTestCase.class})
+@SuiteClasses({ UserManagementTestCase.class, RoleManagementTestCase.class, GroupManagementTestCase.class,
+    UserGroupsRelationshipTestCase.class, UserRolesRelationshipTestCase.class, UserGroupRoleRelationshipTestCase.class,
+    RoleQueryTestCase.class, GroupQueryTestCase.class, UserQueryTestCase.class, CredentialManagementTestCase.class})
 public class LDAPIdentityStoreTestSuite extends AbstractLDAPTest implements TestLifecycle {
 
     private static LDAPIdentityStoreTestSuite instance;
