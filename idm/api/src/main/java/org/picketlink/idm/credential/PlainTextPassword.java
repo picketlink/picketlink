32f43f54fe1cbfb268712e118733c7abe30ab5e2
Anil Saldhana <Anil.Saldhana@jboss.com>
2012-12-31 14:57:23 -0600

    chaining on the setters

--- idm/api/src/main/java/org/picketlink/idm/credential/PlainTextPassword.java
+++ idm/api/src/main/java/org/picketlink/idm/credential/PlainTextPassword.java
@@ -12,6 +12,9 @@ public class PlainTextPassword {
     public PlainTextPassword(char[] value) {
         this.value = value;
     }
+    public PlainTextPassword(String str) {
+        this.value = str.toCharArray();
+    }
 
     public char[] getValue() {
         return value;
