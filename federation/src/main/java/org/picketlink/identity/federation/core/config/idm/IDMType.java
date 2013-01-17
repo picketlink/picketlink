54daacee1414d22f5ea7cef03066c57e91fb82e2
mposolda <mposolda@gmail.com>
2013-01-18 00:05:05 +0100

    Started work on XML parsing of IDM configuration

--- federation/src/main/java/org/picketlink/identity/federation/core/config/idm/IDMType.java
+++ federation/src/main/java/org/picketlink/identity/federation/core/config/idm/IDMType.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2012, Red Hat, Inc., and individual contributors
+ * Copyright 2013, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -19,8 +19,26 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.picketlink.config;
 
-public class Dummy {
-//TODO: get rid soon
+package org.picketlink.identity.federation.core.config.idm;
+
+/**
+ * Type with configuration of IDM
+ *
+ * TODO: Move this class to config module. For now it needs to be in federation because needs to be accessible from PicketlinkType class
+ * TODO: Add XML config snippet similarly like for other type classes
+ *
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class IDMType {
+
+    private String identityManagerClass;
+    private String storeFactoryClass;
+    private String identityStoreInvocationContextFactoryClass;
+    private String eventBridgeClass;
+    private String credentialHandlerFactoryClass;
+    private String identityCacheClass;
+    private String idGeneratorClass;
+
+    private IdentityConfigurationType identityConfigurationType;
 }
