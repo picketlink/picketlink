58ffa34c3f3c7f6f7c09a6a8d4192a71df5b2624
pedroigor <pigor.craveiro@gmail.com>
2013-02-14 13:12:57 -0200

    Ignoring test. It is under construction.

--- idm/tests/src/test/java/org/picketlink/test/idm/relationship/AgentGroupRoleRelationshipTestCase.java
+++ idm/tests/src/test/java/org/picketlink/test/idm/relationship/AgentGroupRoleRelationshipTestCase.java
@@ -21,6 +21,7 @@ package org.picketlink.test.idm.relationship;
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.picketlink.idm.IdentityManager;
 import org.picketlink.idm.model.Agent;
@@ -105,6 +106,7 @@ public class AgentGroupRoleRelationshipTestCase<T extends Agent> extends Abstrac
     }
 
     @Test
+    @Ignore
     public void testGrantParentGroupRole() throws Exception {
         IdentityManager identityManager = getIdentityManager();
         
