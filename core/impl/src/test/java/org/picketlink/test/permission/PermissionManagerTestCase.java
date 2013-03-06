277c7dbb628f73cbb33bf98b5a77b53185872a7f
sbryzak <sbryzak@gmail.com>
2013-03-06 12:28:10 +1000

    use managed JBAS to execute core module tests

--- core/impl/src/test/java/org/picketlink/test/permission/PermissionManagerTestCase.java
+++ core/impl/src/test/java/org/picketlink/test/permission/PermissionManagerTestCase.java
@@ -1,11 +1,18 @@
 package org.picketlink.test.permission;
 
+import java.io.File;
+
 import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.FileAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.picketlink.permission.PermissionManager;
@@ -19,13 +26,24 @@ import org.picketlink.test.permission.schema.Permissions;
 @RunWith(Arquillian.class)
 public class PermissionManagerTestCase {
     @Inject PermissionManager permissionManager;
-    
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class)
                 .addClass(PermissionManagerTestCase.class)
                 .addClass(Permissions.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-                .addAsManifestResource(EmptyAsset.INSTANCE, "persistence.xml");
+                .addAsManifestResource(new FileAsset(new File("src/test/resources/META-INF/beans.xml")), "beans.xml")
+                .addAsManifestResource(new FileAsset(new File("src/test/resources/META-INF/persistence.xml")), "persistence.xml")
+                .addAsLibraries(
+                        DependencyResolvers.use(MavenDependencyResolver.class)
+                        .artifact("org.picketlink:picketlink-core-api:3.0.0-SNAPSHOT")
+                        .artifact("org.picketlink:picketlink-core-impl:3.0.0-SNAPSHOT")
+                        .resolveAsFiles()
+                        );
+
+        System.out.println(war.toString(true));
+
+        return war;
     }
 
     @Test
