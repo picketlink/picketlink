e7ca47a54e39239b49642076588d88f4cf7d435b
pedroigor <pigor.craveiro@gmail.com>
2013-03-08 16:14:00 -0300

    Initial code for performance tests.

---
+++ idm/tests/src/test/java/org/picketlink/test/idm/performance/LoadUsersJMeterTest.java
@@ -0,0 +1,144 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.picketlink.test.idm.performance;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.config.FeatureSet;
+import org.picketlink.idm.config.IdentityConfiguration;
+import org.picketlink.idm.file.internal.FileDataSource;
+import org.picketlink.idm.file.internal.FileIdentityStoreConfiguration;
+import org.picketlink.idm.internal.DefaultIdentityManager;
+import org.picketlink.idm.internal.DefaultIdentityStoreInvocationContextFactory;
+import org.picketlink.idm.model.Authorization;
+import org.picketlink.idm.model.Realm;
+import org.picketlink.idm.model.SimpleUser;
+import org.picketlink.test.idm.relationship.CustomRelationship;
+
+/**
+ * @author Pedro Silva
+ * 
+ */
+public class LoadUsersJMeterTest extends AbstractJavaSamplerClient {
+
+    private static IdentityManager identityManager = null;
+
+    static {
+        identityManager = createIdentityManager();
+    }
+
+    @Override
+    public Arguments getDefaultParameters() {
+        Arguments arguments = new Arguments();
+        
+        arguments.addArgument("loginName", "Sample User");
+        
+        return arguments;
+    }
+    
+    @Override
+    public void setupTest(JavaSamplerContext context) {
+    }
+
+    @Override
+    public void teardownTest(JavaSamplerContext context) {
+    }
+
+    @Override
+    public SampleResult runTest(JavaSamplerContext context) {
+        SampleResult result = new SampleResult();
+
+        result.sampleStart();
+
+        boolean success = false;
+
+        String loginName = context.getParameter("loginName");
+        
+        if (loginName == null) {
+            loginName = "Sample User";
+        }
+
+        JMeterVariables vars = JMeterContextService.getContext().getVariables();
+        
+        vars.put("loginName", loginName);
+
+        try {
+            performUserLoadTest(loginName);
+            success = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            result.sampleEnd();
+            result.setSuccessful(success);
+        }
+
+        return result;
+    }
+
+    private void performUserLoadTest(String loginName) {
+        identityManager.add(new SimpleUser(loginName));
+    }
+
+    private static IdentityManager createIdentityManager() {
+        IdentityConfiguration config = new IdentityConfiguration();
+
+        FileDataSource dataSource = new FileDataSource();
+
+        dataSource.setAlwaysCreateFiles(false);
+        dataSource.setAsyncWrite(true);
+        dataSource.setAsyncThreadPool(50);
+
+        addDefaultConfiguration(config, dataSource);
+
+        IdentityManager identityManager = new DefaultIdentityManager();
+
+        identityManager.bootstrap(config, new DefaultIdentityStoreInvocationContextFactory(null));
+
+        return identityManager;
+    }
+
+    private static void addDefaultConfiguration(IdentityConfiguration config, FileDataSource dataSource) {
+        FileIdentityStoreConfiguration configuration = new FileIdentityStoreConfiguration();
+
+        // add the realms that should be supported by the file store
+        configuration.addRealm(Realm.DEFAULT_REALM);
+        configuration.addRealm("Testing");
+
+        configuration.setDataSource(dataSource);
+
+        FeatureSet.addFeatureSupport(configuration.getFeatureSet());
+        FeatureSet.addRelationshipSupport(configuration.getFeatureSet());
+        FeatureSet.addRelationshipSupport(configuration.getFeatureSet(), CustomRelationship.class);
+        FeatureSet.addRelationshipSupport(configuration.getFeatureSet(), Authorization.class);
+        configuration.getFeatureSet().setSupportsCustomRelationships(true);
+        configuration.getFeatureSet().setSupportsMultiRealm(true);
+
+        config.addStoreConfiguration(configuration);
+    }
+
+}
---
+++ idm/tests/src/test/java/org/picketlink/test/idm/performance/ReadUsersJMeterTest.java
@@ -0,0 +1,118 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.picketlink.test.idm.performance;
+
+import java.util.List;
+
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.config.FeatureSet;
+import org.picketlink.idm.config.IdentityConfiguration;
+import org.picketlink.idm.file.internal.FileDataSource;
+import org.picketlink.idm.file.internal.FileIdentityStoreConfiguration;
+import org.picketlink.idm.internal.DefaultIdentityManager;
+import org.picketlink.idm.internal.DefaultIdentityStoreInvocationContextFactory;
+import org.picketlink.idm.model.Authorization;
+import org.picketlink.idm.model.Realm;
+import org.picketlink.idm.model.User;
+import org.picketlink.idm.query.IdentityQuery;
+import org.picketlink.test.idm.relationship.CustomRelationship;
+
+/**
+ * @author Pedro Silva
+ * 
+ */
+public class ReadUsersJMeterTest extends AbstractJavaSamplerClient {
+
+    private static IdentityManager identityManager = null;
+
+    static {
+        identityManager = createIdentityManager();
+    }
+
+    @Override
+    public SampleResult runTest(JavaSamplerContext context) {
+        SampleResult result = new SampleResult();
+
+        result.sampleStart();
+
+        boolean success = false;
+
+        try {
+            IdentityQuery<User> query = identityManager.createIdentityQuery(User.class);
+            
+            query.setParameter(User.ENABLED, true);
+            
+            List<User> users = query.getResultList();
+
+            // TODO: return test results.
+            
+            success = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            result.sampleEnd();
+            result.setSuccessful(success);
+        }
+
+        return result;
+    }
+
+    private static IdentityManager createIdentityManager() {
+        IdentityConfiguration config = new IdentityConfiguration();
+
+        FileDataSource dataSource = new FileDataSource();
+
+        dataSource.setAlwaysCreateFiles(false);
+
+        addDefaultConfiguration(config, dataSource);
+
+        IdentityManager identityManager = new DefaultIdentityManager();
+
+        identityManager.bootstrap(config, new DefaultIdentityStoreInvocationContextFactory(null));
+
+        return identityManager;
+    }
+
+    private static void addDefaultConfiguration(IdentityConfiguration config, FileDataSource dataSource) {
+        FileIdentityStoreConfiguration configuration = new FileIdentityStoreConfiguration();
+
+        // add the realms that should be supported by the file store
+        configuration.addRealm(Realm.DEFAULT_REALM);
+        configuration.addRealm("Testing");
+
+        configuration.setDataSource(dataSource);
+
+        FeatureSet.addFeatureSupport(configuration.getFeatureSet());
+        FeatureSet.addRelationshipSupport(configuration.getFeatureSet());
+        FeatureSet.addRelationshipSupport(configuration.getFeatureSet(), CustomRelationship.class);
+        FeatureSet.addRelationshipSupport(configuration.getFeatureSet(), Authorization.class);
+        configuration.getFeatureSet().setSupportsCustomRelationships(true);
+        configuration.getFeatureSet().setSupportsMultiRealm(true);
+
+        config.addStoreConfiguration(configuration);
+    }
+
+}
