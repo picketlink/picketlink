c9cbdfd829b8abe4645b4d243b02391fcf499bdc
pedroigor <pigor.craveiro@gmail.com>
2013-05-28 17:07:56 -0300

    [PLINK-160] - Picketlink forwards to /hosted for resources without auth-constraints.

--- modules/config/src/main/java/org/picketlink/config/federation/IDPType.java
+++ modules/config/src/main/java/org/picketlink/config/federation/IDPType.java
@@ -1,23 +1,28 @@
 /*
- * JBoss, Home of Professional Open Source
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
  *
- * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.picketlink.config.federation;
+package org.picketlink.identity.federation.core.config;
 
-import org.picketlink.common.util.StringUtil;
+import org.picketlink.identity.federation.core.util.StringUtil;
+import org.picketlink.identity.federation.web.core.IdentityParticipantStack;
 
 /**
  * 
@@ -57,6 +62,8 @@ public class IDPType extends ProviderType {
     protected String attributeManager;
 
     protected Boolean encrypt;
+    
+    protected String hostedURI;
 
     /**
      * SAML Web Browser SSO Profile has a requirement that the IDP does not respond back in Redirect Binding. Set this to false
@@ -159,6 +166,24 @@ public class IDPType extends ProviderType {
      */
     public void setEncrypt(Boolean value) {
         this.encrypt = value;
+    }
+
+    /**
+     * Gets the value of the encrypt property.
+     * 
+     * @return possible object is {@link String}
+     */
+    public String getHostedURI() {
+        return hostedURI;
+    }
+
+    /**
+     * Sets the value of the hostedURI property.
+     * 
+     * @param value allowed object is {@link String}
+     */
+    public void setHostedURI(String hostedURI) {
+        this.hostedURI = hostedURI;
     }
 
     /**
