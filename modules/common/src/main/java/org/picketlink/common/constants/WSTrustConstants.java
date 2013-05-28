33dd6dfee8187f5ac9f1e1e5ef79576dbe650acb
Anil Saldhana <Anil.Saldhana@jboss.com>
2013-05-28 10:09:08 -0500

    PLINK-44: STS parsing should not choke on ws-p 1.5 tags

--- modules/common/src/main/java/org/picketlink/common/constants/WSTrustConstants.java
+++ modules/common/src/main/java/org/picketlink/common/constants/WSTrustConstants.java
@@ -74,6 +74,8 @@ public interface WSTrustConstants {
 
     String WSP_NS = "http://schemas.xmlsoap.org/ws/2004/09/policy";
 
+    String WSP_15_NS = "http://www.w3.org/ns/ws-policy";
+
     String WSU_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
 
     String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
