4a78fc2e09677e7629fe8e35827a2c941e043903
pedroigor <pigor.craveiro@gmail.com>
2013-05-29 21:35:27 -0300

    Sample entity model for the JPA store testing.

---
+++ modules/idm/schema/src/main/java/org/picketlink/idm/jpa/schema/sample/Application.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.picketlink.idm.jpa.schema.sample;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.picketlink.idm.jpa.annotations.Partition;
+
+/**
+ * @author pedroigor
+ */
+@Entity
+@Partition
+public class Application {
+
+    @Id
+    private String id;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!getClass().isInstance(obj)) {
+            return false;
+        }
+
+        Application other = (Application) obj;
+
+        return getId() != null && other.getId() != null && getId().equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getId() != null ? getId().hashCode() : 0);
+        return result;
+    }
+}
