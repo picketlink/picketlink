0f80640169600b91b6faf148420d3d4ce5d279d8
Pedro Igor <pedroigor@localhost.localdomain>
2013-07-31 12:31:25 -0300

    Added a enum with different match options when using the TypedPropertyCriteria.

--- modules/common/src/main/java/org/picketlink/common/properties/query/TypedPropertyCriteria.java
+++ modules/common/src/main/java/org/picketlink/common/properties/query/TypedPropertyCriteria.java
@@ -28,33 +28,72 @@ import java.lang.reflect.Method;
  */
 public class TypedPropertyCriteria implements PropertyCriteria 
 {
+
+    /**
+     * <p>
+     *     Different options can be used to match a specific property based on its type. Regardless of the
+     *     option chosen, if the property type equals the <code>propertyClass</code> it will be selected.
+     *
+     *     <ul>
+     *         <li>SUB_TYPE: Also consider properties where its type is a subtype of
+     *         <code>propertyClass</code>.
+     *         .</li>
+     *         <li>SUPER_TYPE: Also consider properties where its type is a superclass or superinterface of
+     *         <code>propertyClass</code>.
+     *         .</li>
+     *     </ul>
+     * </p>
+     */
+    public static enum MatchOption {SUB_TYPE, SUPER_TYPE, ALL}
+
     private final Class<?> propertyClass;
-    private final boolean selectSubTypes;
+    private final MatchOption matchOption;
 
     public TypedPropertyCriteria(Class<?> propertyClass) 
     {
-        this(propertyClass, false);
+        this(propertyClass, null);
     }
 
-    public TypedPropertyCriteria(Class<?> propertyClass, boolean selectSubTypes)
+    public TypedPropertyCriteria(Class<?> propertyClass, MatchOption matchOption)
     {
+        if (propertyClass == null) {
+            throw new IllegalArgumentException("Property class can not be null.");
+        }
         this.propertyClass = propertyClass;
-        this.selectSubTypes = selectSubTypes;
+        this.matchOption = matchOption;
     }
 
     public boolean fieldMatches(Field f) 
     {
-        return propertyClass.equals(f.getType());
+        return match(f.getType());
     }
 
     public boolean methodMatches(Method m) 
     {
-        boolean equals = propertyClass.equals(m.getReturnType());
+        return match(m.getReturnType());
+    }
 
-        if (!equals && selectSubTypes) {
-            equals = propertyClass.isAssignableFrom(m.getReturnType());
+    private boolean match(Class<?> type) {
+        if (propertyClass.equals(type)) {
+            return true;
+        } else {
+            boolean matchSubType = propertyClass.isAssignableFrom(type);
+
+            if (MatchOption.SUB_TYPE == this.matchOption) {
+                return matchSubType;
+            }
+
+            boolean matchSuperType = type.isAssignableFrom(propertyClass);
+
+            if (MatchOption.SUPER_TYPE == this.matchOption) {
+                return matchSuperType;
+            }
+
+            if (MatchOption.ALL == this.matchOption) {
+                return matchSubType || matchSuperType;
+            }
         }
 
-        return equals;
+        return false;
     }
 }
