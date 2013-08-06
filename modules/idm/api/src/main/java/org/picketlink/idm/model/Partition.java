30c161e3dc5230efdfd7a6ee457a6d41bfbbf9b9
sbryzak <sbryzak@gmail.com>
2013-08-06 18:45:13 +1000

    removed redundant getId() method

--- modules/idm/api/src/main/java/org/picketlink/idm/model/Partition.java
+++ modules/idm/api/src/main/java/org/picketlink/idm/model/Partition.java
@@ -27,13 +27,6 @@ package org.picketlink.idm.model;
 public interface Partition extends AttributedType {
 
     /**
-     * Returns the unique identifier for the partition
-     *
-     * @return String value representing the unique identifier
-     */
-    String getId();
-
-    /**
      * Returns the name of the partition.  The partition name is unique for the
      * type of partition.
      *
