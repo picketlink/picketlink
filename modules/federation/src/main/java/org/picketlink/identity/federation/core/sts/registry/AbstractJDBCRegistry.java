5cf15584f78d00df16c81799a375e927126fd3ca
Pedro Igor <pedroigor@localhost.localdomain>
2013-08-09 18:26:48 -0300

    [PLINK-232] - JDBC Token and Revocation Registry.

---
+++ modules/federation/src/main/java/org/picketlink/identity/federation/core/sts/registry/AbstractJDBCRegistry.java
@@ -0,0 +1,87 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketlink.identity.federation.core.sts.registry;
+
+import org.picketlink.common.PicketLinkLogger;
+import org.picketlink.common.PicketLinkLoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Anil Saldhana
+ * @since August 06, 2013
+ */
+public class AbstractJDBCRegistry {
+    protected static final PicketLinkLogger logger = PicketLinkLoggerFactory.getLogger();
+    protected DataSource dataSource;
+
+    public AbstractJDBCRegistry() {
+        this("jdbc/picketlink-sts");
+    }
+
+    public AbstractJDBCRegistry(String jndiName) {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            dataSource = (DataSource) envContext.lookup(jndiName);
+            if(dataSource == null){
+                throw logger.datasourceIsNull();
+            }
+        } catch (NamingException e) {
+            throw logger.jbdcInitializationError(e);
+        }
+    }
+
+    protected void safeClose(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+            }
+        }
+    }
+
+    protected void safeClose(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+            }
+        }
+    }
+
+    protected void safeClose(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+            }
+        }
+    }
+}
