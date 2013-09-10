c8ff9c7140ea2e1af058bede2909e1428e28e10c
sbryzak <sbryzak@gmail.com>
2013-09-10 21:46:34 +1000

    PLINK-268 add PermissionManager interface

---
+++ modules/idm/api/src/main/java/org/picketlink/idm/PermissionManager.java
@@ -0,0 +1,75 @@
+package org.picketlink.idm;
+
+import java.util.List;
+
+import org.picketlink.idm.permission.Permission;
+
+/**
+ * Manages all Permission Management related operations.
+ *
+ * @author Shane Bryzak
+ */
+public interface PermissionManager {
+    /**
+     * Return a list of all permissions for the specified resource.
+     *
+     * @param resource
+     * @return
+     */
+    List<Permission> listPermissions(Object resource);
+
+    /**
+     * Return a list of all permissions for the specified resource, of the specified permission type
+     * @param resource
+     * @param permission
+     * @return
+     */
+    List<Permission> listPermissions(Object resource, String permission);
+
+    /**
+     * Grant the specified permission
+     * @param permission
+     *
+     * @return boolean returns true if the permission was granted successfully
+     */
+    boolean grantPermission(Permission permission);
+
+    /**
+     * Grant all permissions in the specified list
+     *
+     * @param permissions
+     * @return boolean returns true if the permissions were granted successfully
+     */
+    boolean grantPermissions(List<Permission> permissions);
+
+    /**
+     * Revoke the specified permission
+     *
+     * @param permission
+     * @return
+     */
+    public boolean revokePermission(Permission permission);
+
+    /**
+     * Revoke the specified permissions
+     *
+     * @param permissions
+     * @return
+     */
+    public boolean revokePermissions(List<Permission> permissions);
+
+    /**
+     * Remove all permissions for the specified resource
+     *
+     * @param resource
+     */
+    public void clearPermissions(Object resource);
+
+    /**
+     * List all allowed permission type strings for the specified resource
+     *
+     * @param resource
+     * @return
+     */
+    public List<String> listPermissionTypes(Object resource);
+}
