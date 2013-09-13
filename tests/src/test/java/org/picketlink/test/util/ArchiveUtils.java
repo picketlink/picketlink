94e5bf6af028c1b9a0849463a32345b98784c663
Pedro Igor <pigor.craveiro@gmail.com>
2013-09-12 21:40:53 -0300

    Next release version 2.5.2-SNAPSHOT.

--- tests/src/test/java/org/picketlink/test/util/ArchiveUtils.java
+++ tests/src/test/java/org/picketlink/test/util/ArchiveUtils.java
@@ -88,7 +88,7 @@ public class ArchiveUtils {
      * @return
      */
     public static String getCurrentProjectVersion() {
-        return System.getProperty("project.version", "2.5.0-SNAPSHOT");
+        return System.getProperty("project.version");
     }
 
 }
