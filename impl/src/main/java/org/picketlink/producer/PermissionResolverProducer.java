7a70c179a5b2f4eec3b5951e43ef79541713535a
sbryzak <sbryzak@gmail.com>
2013-10-16 12:45:45 +1000

    PLINK-268 add PermissionResolver producer method

---
+++ impl/src/main/java/org/picketlink/producer/PermissionResolverProducer.java
@@ -0,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.picketlink.producer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+import org.picketlink.idm.permission.PermissionResolver;
+import org.picketlink.idm.permission.spi.PermissionVoter;
+
+/**
+ * Defines the producer method for the application's PermissionResolver instance
+ *
+ * @author Shane Bryzak
+ */
+@ApplicationScoped
+public class PermissionResolverProducer {
+
+    private PermissionResolver resolver;
+
+    @Inject 
+    public void init(Instance<PermissionVoter> votersInstance) {
+        List<PermissionVoter> voters = new ArrayList<PermissionVoter>();
+
+        // TODO populate the voters
+
+        resolver = new PermissionResolver(voters);
+    }
+
+
+    @Produces
+    public PermissionResolver createPermissionResolver() {
+        return resolver;
+    }
+}
