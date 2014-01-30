0040553db4eaea1ed5bd17e6dd5d1d43b53aaf5e
sbryzak <sbryzak@gmail.com>
2014-01-30 12:33:44 +1000

    PLINK-268 configuration support for registered identity types and relationship types

--- modules/idm/api/src/main/java/org/picketlink/idm/config/IdentityConfiguration.java
+++ modules/idm/api/src/main/java/org/picketlink/idm/config/IdentityConfiguration.java
@@ -18,9 +18,21 @@
 
 package org.picketlink.idm.config;
 
+import org.picketlink.idm.model.IdentityType;
+import org.picketlink.idm.model.Relationship;
+import org.picketlink.idm.model.basic.Agent;
+import org.picketlink.idm.model.basic.Grant;
+import org.picketlink.idm.model.basic.Group;
+import org.picketlink.idm.model.basic.GroupMembership;
+import org.picketlink.idm.model.basic.GroupRole;
+import org.picketlink.idm.model.basic.Role;
+import org.picketlink.idm.model.basic.User;
 import org.picketlink.idm.spi.RelationshipPolicy;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.Collections.unmodifiableList;
 
@@ -33,11 +45,37 @@ import static java.util.Collections.unmodifiableList;
  */
 public class IdentityConfiguration {
 
+    @SuppressWarnings("rawtypes")
+    private static final Class[] DEFAULT_IDENTITY_TYPES = {
+        Agent.class,
+        User.class,
+        Group.class,
+        Role.class
+    };
+
+    @SuppressWarnings("rawtypes")
+    private static final Class[] DEFAULT_RELATIONSHIP_TYPES = {
+        Grant.class,
+        GroupMembership.class,
+        GroupRole.class
+    };
+
     private final String name;
     private final List<? extends IdentityStoreConfiguration> storeConfiguration;
     private final RelationshipPolicy relationshipPolicy;
 
-    IdentityConfiguration(String name, List<? extends IdentityStoreConfiguration> identityStores, RelationshipPolicy relationshipPolicy) {
+    private final Set<Class<? extends IdentityType>> registeredIdentityTypes;
+    private final Set<Class<? extends Relationship>> registeredRelationshipTypes;
+
+    IdentityConfiguration(String name, List<? extends IdentityStoreConfiguration> identityStores,
+            RelationshipPolicy relationshipPolicy) {
+        this(name, identityStores, relationshipPolicy, null, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    IdentityConfiguration(String name, List<? extends IdentityStoreConfiguration> identityStores,
+            RelationshipPolicy relationshipPolicy, Set<Class<? extends IdentityType>> registeredIdentityTypes,
+            Set<Class<? extends Relationship>> registeredRelationshipTypes) {
         if (name == null) {
             throw new SecurityConfigurationException("You must specify a name for the IdentityConfiguration.");
         }
@@ -45,6 +83,14 @@ public class IdentityConfiguration {
         this.name = name;
         this.storeConfiguration = unmodifiableList(identityStores);
         this.relationshipPolicy = relationshipPolicy;
+
+        this.registeredIdentityTypes = registeredIdentityTypes.isEmpty() || registeredIdentityTypes == null ?
+                 new HashSet<Class<? extends IdentityType>>(Arrays.<Class<? extends IdentityType>>asList(DEFAULT_IDENTITY_TYPES))
+                 : registeredIdentityTypes;
+
+        this.registeredRelationshipTypes = registeredRelationshipTypes.isEmpty() || registeredRelationshipTypes == null ?
+                new HashSet<Class<? extends Relationship>>(Arrays.<Class<? extends Relationship>>asList(DEFAULT_RELATIONSHIP_TYPES))
+                : registeredRelationshipTypes;
     }
 
     public RelationshipPolicy getRelationshipPolicy() {
@@ -67,6 +113,14 @@ public class IdentityConfiguration {
         }
 
         return false;
+    }
+
+    public Set<Class<? extends IdentityType>> getRegisteredIdentityTypes() {
+        return registeredIdentityTypes;
+    }
+
+    public Set<Class<? extends Relationship>> getRegisteredRelationshipTypes() {
+        return registeredRelationshipTypes;
     }
 
     @Override
