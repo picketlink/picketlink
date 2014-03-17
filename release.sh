2f7524c53441d4a9368a60dfeafae855d885fcbb
Pedro Igor <pigor.craveiro@gmail.com>
2014-03-17 12:36:55 -0300

    Updating release script.

--- release.sh
+++ release.sh
@@ -30,7 +30,6 @@ check_release_version() {
 clean_local_repo() {
     git clean -f -d
     git reset --hard upstream/master
-    rm -rf release.properties
     rm -rf $RELEASE_LOG_FILE
 }
 
@@ -49,7 +48,7 @@ rollback() {
     git tag -d v$RELEASE_VERSION
     git push origin :refs/tags/v$RELEASE_VERSION
     git push upstream :refs/tags/v$RELEASE_VERSION
-    echo "Done."    
+    echo "Done."
 }
 
 # upload the docs to jboss.org/docs
@@ -91,13 +90,9 @@ release() {
 	echo "Preparing local repository to release."
 	execute_cmd git checkout master
 	echo "    Fetching latest changes from upstream..."
-	execute_cmd git fetch upstream
-	echo "    Merging latest changes into master..."
-	execute_cmd git merge upstream/master
-	echo "    Swithing to develop branch..."
-	execute_cmd git checkout develop
-	echo "    Merging latest changes into develop branch..."
-	execute_cmd git merge master
+	execute_cmd git fetch upstream master
+	echo "    Pulling changes from master..."
+	execute_cmd git pull upstream master
 	echo "Done."
 	echo ""
 
@@ -119,40 +114,30 @@ release() {
 
 	echo "Preparing to release using git flow."
 	echo "    Starting release $RELEASE_VERSION..."
-	execute_cmd git flow release start $RELEASE_VERSION
+	execute_cmd git checkout -b release/v$RELEASE_VERSION
 	echo "Done."
 	echo ""
 
 	FLAG_PERFORM_RELEASE="false"
 
-	echo "Preparing to release."
-	echo "    Executing maven-release-plugin in DryRun mode..."
-	execute_cmd mvn -DpreparationGoals="-Drelease -Prelease -DskipTests=true clean install" release:prepare	--batch-mode -DdevelopmentVersion=$DEVELOPMENT_VERSION -DreleaseVersion=$RELEASE_VERSION -Dtag=vRELEASE_VERSION	-DdryRun -DignoreSnapshots=true -Prelease
-	if check_build_result; then     
-		 read -p "Project is ready to release. Do you want to proceed ?[y/n] " FLAG_PERFORM_RELEASE
-	else
-		 echo "ERROR: Project build failed. Can not proceed with the release. Check the logs."
-		 exit 1;
-	fi
+    echo "Releasing version $RELEASE_VERSION."
+    execute_cmd perl -pi -e 's/'$DEVELOPMENT_VERSION'/'$RELEASE_VERSION'/g' `find . -name pom.xml`
+    execute_cmd cd maven-plugins/picketlink-jdocbook-style/
+    execute_cmd mvn -Prelease -DskipTests=true clean install
+    execute_cmd cd ../../
+    execute_cmd mvn -Prelease -DskipTests=true clean install
+    if check_build_result; then
+        echo "Done."
+    else
+        echo "ERROR: Release failed."
+        exit 1
+    fi
 
-	if [ "$FLAG_PERFORM_RELEASE" == "y" ]; then
-		echo "Releasing version $RELEASE_VERSION."
-		cd maven-plugins/picketlink-jdocbook-style/
-		execute_cmd perl -pi -e 's/'$DEVELOPMENT_VERSION'/'$RELEASE_VERSION'/g' `find . -name pom.xml`
-		execute_cmd mvn clean install
-		execute_cmd perl -pi -e 's/'$RELEASE_VERSION'/'$DEVELOPMENT_VERSION'/g' `find . -name pom.xml`
-		cd ../../
-		execute_cmd mvn -DpreparationGoals="-Drelease -Prelease clean install" release:prepare --batch-mode -DdevelopmentVersion=$DEVELOPMENT_VERSION -DreleaseVersion=$RELEASE_VERSION -Dtag=v$RELEASE_VERSION -Dresume=false -DignoreSnapshots=true -Prelease
-		if check_build_result; then
-			echo "Done."
-		else
-			echo "ERROR: Release failed."
-			exit 1
-		fi
-	else
-                rollback
-		exit 1
-	fi
+    execute_cmd git add .
+    execute_cmd git commit -m "Release $RELEASE_VERSION."
+    execute_cmd git tag -a v$RELEASE_VERSION -m "Version $RELEASE_VERSION."
+    execute_cmd git push --tags upstream
+
 	echo ""
 
 	FLAG_PUBLISH_NEXUS="n"
@@ -160,7 +145,7 @@ release() {
 
 	if [ "$FLAG_PUBLISH_NEXUS" == "y" ]; then
 		echo "Publishing artifacts to Nexus. "
-		execute_cmd mvn release:perform nexus:staging-close -Prelease
+		execute_cmd mvn -Prelease -DskipTests=true clean source:jar deploy
 		echo "Done. You can now go to Nexus and finish release the artifacts."
 	fi
 	echo ""
@@ -178,9 +163,8 @@ release() {
 	echo "Finishing the release."
 	git flow release finish $RELEASE_VERSION
         clean_local_repo
-        git branch -D release/$RELEASE_VERSION
-        git push origin :release/$RELEASE_VERSION
-        git push upstream :release/$RELEASE_VERSION
+        git checkout master
+        git branch -D release/v$RELEASE_VERSION
         git reset --hard upstream/master
 	echo "Done."
 	exit 0
@@ -211,7 +195,7 @@ while true; do
 		 usage
 		 exit
 		 ;;
-	 --snapshot )	     
+	 --snapshot )
 		 DEVELOPMENT_VERSION=$2
 		 shift
 		 shift
@@ -229,11 +213,11 @@ while true; do
 		 upload_docs
                  exit 0
                  ;;
-	 -- ) 
-	     shift; 
+	 -- )
+	     shift;
 	     break ;;
-     * ) 
-         break 
+     * )
+         break
          ;;
   esac
 done
