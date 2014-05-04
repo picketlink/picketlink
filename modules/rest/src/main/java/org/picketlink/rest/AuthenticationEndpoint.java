95a751f7ea3569f3953133266ea69621d7819a84
sbryzak <sbryzak@gmail.com>
2014-05-05 09:12:14 +1000

    PLINK-155 implemented basic authentication endpoint

---
+++ modules/rest/src/main/java/org/picketlink/rest/AuthenticationEndpoint.java
@@ -0,0 +1,56 @@
+package org.picketlink.rest;
+
+import java.util.Map;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.picketlink.Identity;
+import org.picketlink.Identity.AuthenticationResult;
+import org.picketlink.credential.DefaultLoginCredentials;
+import org.picketlink.idm.model.Account;
+
+/**
+ * Provides RESTful authentication services
+ *
+ * @author Shane Bryzak
+ */
+@Path("/auth")
+@RequestScoped
+public class AuthenticationEndpoint {
+
+    @Inject
+    DefaultLoginCredentials credentials;
+
+    @Inject
+    Identity identity;
+
+    /**
+     * Authenticates using the username and password values passed as parameters.
+     * Returns the authenticated Account instance
+     *
+     * @param params
+     * @return
+     */
+    @POST
+    @Path("/login")
+    @Consumes("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Account login(Map<String,String> params) {
+        if (!identity.isLoggedIn()) {
+            credentials.setUserId(params.get("username"));
+            credentials.setPassword(params.get("password"));
+
+            if (!AuthenticationResult.SUCCESS.equals(identity.login())) {
+                return null;
+            }
+        }
+
+        return identity.getAccount();
+    }
+}
--- modules/rest/src/main/js/picketlink.js
+++ modules/rest/src/main/js/picketlink.js
@@ -1,54 +1,45 @@
-pl = {};
-
-pl.createHttpRequest = function(mimeType) {
-  if (window.XMLHttpRequest) {
-    var req = new XMLHttpRequest();
-    if (mimeType !== null && req.overrideMimeType) {
-      req.overrideMimeType(mimeType);
+var pl = {
+  loggedIn: false,
+  account: null,
+  basePath: "",
+  createRequestObject: function(callback) {
+    var r;
+    if (window.XMLHttpRequest) {
+      r = new XMLHttpRequest();
+    } else {
+      r = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    return req;
+    r.onreadystatechange = function() {
+      if (r.readyState == 4) {
+        if (r.status >= 200 && r.status <= 299) {
+          // Done to avoid a memory leak
+          window.setTimeout(function() {
+            r.onreadystatechange = function() {};
+          }, 0);
+          if (callback) {
+            callback(r.responseText);
+          }
+        }
+      }
+    }
+    return r;
+  },
+  login: function(username, password, callback) {
+    var cb = function(response) {
+      if (typeof response == "string" && response.length > 0) {
+        var acct = JSON.parse(response);
+        if (acct != null) {
+          pl.loggedIn = true;
+          pl.account = acct;
+          if (callback) {
+            callback.call();
+          }
+        }
+      }
+    }
+    var r = pl.createRequestObject(cb);
+    r.open("POST", pl.basePath + "/auth/login", true);
+    r.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    r.send(JSON.stringify({username: username, password: password}));
   }
-  else {
-    return new ActiveXObject("Microsoft.XMLHTTP");
-  }
-};
-
-pl.addUser = function(u) {
-
-};
-
-pl.addGroup = function() {
-
-};
-
-pl.addRole = function() {
-
-};
-
-pl.updateUser = function(u) {
-
-};
-
-pl.updateGroup = function(u) {
-
-};
-
-pl.updateRole = function(u) {
-
-};
-
-pl.deleteUser = function(id) {
-
-};
-
-pl.deleteGroup = function(id) {
-
-};
-
-pl.deleteRole = function(id) {
-
-};
-
-pl.createQuery = function() {
-
 };
